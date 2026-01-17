cd51dc99916bc31496b6af84b6c38f0d843230f6
bururun <bururun@users.noreply.github.com>
2026-01-18 01:01:02 +0200

    refactor(ui): fix null pointer exception

---
+++ lib/helpers.py
@@ -0,0 +1,9 @@
+# Helper functions
+
+def helper_function_4(x):
+    """Helper function for iteration 4."""
+    return x * 4
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
